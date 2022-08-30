18164c8b92a247d59517fa915a03ccf7298c6b25
vinitapp <vinitaprabhu10@gmail.com>
2022-08-30 19:57:06 +1000

    different method

--- Develop/script.js
+++ Develop/script.js
@@ -40,3 +40,44 @@ function generatePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
+
+
+
+
+
+
+// Assignment code here
+
+
+// Get references to the #generate element- starter code
+var generateBtn = document.querySelector("#generate");
+
+//Variables to call functions- my code
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
+// Special characters 
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+// Numeric characters
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Alphabetical characters
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+
+
+// Write password to the #password input-starter code
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button-starter code
+generateBtn.addEventListener("click", writePassword);
